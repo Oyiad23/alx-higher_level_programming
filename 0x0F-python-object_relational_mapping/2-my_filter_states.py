470295e7de0b77d9e0c28ae68f7b2dff31e83c80
Oyiad23 <heavenonearth.boakye11@gmail.com>
2024-04-28 21:28:30 -0700

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,19 +10,19 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-        """
-            Access to the database and get the states
-                from the database.
-                    """
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-                        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                                                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-                            cur = db.cursor()
-                                cur.execute("SELECT * FROM states \
-                                                         WHERE name LIKE BINARY '{}' \
-                                                                          ORDER BY states.id ASC".format(argv[4]))
-                                    rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
 
-                                        for row in rows:
-                                                    print(row)
+    for row in rows:
+        print(row)
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,39 +4,25 @@ This script takes in an argument and
 displays all values in the states
 where `name` matches the argument
 from the database `hbtn_0e_0_usa`.
-
-This time the script is safe from
-MySQL injections!
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-        """
-            Access to the database and get the states
-                from the database.
-                    """
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-                        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                                                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-                            with db.cursor() as cur:
-                                        cur.execute("""
-                                                    SELECT
-                                                                    *
-                                                                                FROM
-                                                                                                states
-                                                                                                            WHERE
-                                                                                                                            name LIKE BINARY %(name)s
-                                                                                                                                        ORDER BY
-                                                                                                                                                        states.id ASC
-                                                                                                                                                                """, {
-                                                                                                                                                                                'name': argv[4]
-                                                                                                                                                                                        })
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
 
-                                                                                                                                                                        rows = cur.fetchall()
-
-                                                                                                                                                                            if rows is not None:
-                                                                                                                                                                                        for row in rows:
-                                                                                                                                                                                                        print(row)
+    for row in rows:
+        print(row)
